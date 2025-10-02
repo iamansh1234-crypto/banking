b8c1e74475f2fabbeb04602f0166f49951b4ffbd
iamansh1234-crypto <singhanshrajput5@gmail.com>
2025-10-02 10:17:17 +0530

    feat: add new banking features and UI components
    - Added new pages for My Banks, Payment Transfer, and Transaction History.
    - Implemented AnimatedCounter component for displaying animated numerical values.
    - Created BankCard component to display bank account information.
    - Developed Copy component for copying text to clipboard with feedback.
    - Introduced DoughnutChart component for visualizing bank balances.
    - Enhanced HeaderBox and TotalBalanceBox components for better user experience.
    - Integrated MobileNav and Sidebar components for improved navigation.
    - Updated dependencies in package.json for better performance and features.
    - Refactored postcss.config.mjs to include autoprefixer and tailwindcss.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,11 @@
 export const dynamic = 'force-dynamic'
 
 
+import Sidebar from "@/components/Sidebar";
 import "../globals.css";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 
 
 
@@ -10,10 +14,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: "Ansh" , lastName: "Singh"};
+   if(!loggedIn) redirect('/sign-in')
+
   return (
-   <main>
-    Sidebar
+  <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn}  />
+       <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
         {children}
+      </div>
    </main>
   );
 }
+//1.07.06
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -57,16 +57,32 @@
 // export default Home\
 
 
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Ansh ', lastName: 'Singh', email: 'contact@ansh.com'};
   return ( 
   <section className="home">
         <div className="home-content">
           <header className="home-header">
-            Welcome , User!
+            <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox  accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
           </header>
+          Recent
       </div>
+       <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 1250},{currentBalance: 2500}]}
+      />
   </section>
   )
 }
